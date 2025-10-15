dd0345bc316dc0775f9f8b38f775c23d947cb1e3
Eric Popelka <eric@syncsmart.io>
2025-10-15 12:45:52 -0500

    Initial Commit

---
+++ simple-income-tax-calculator/simple_income_tax_calculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  simple_income_tax_calculatorApp.swift
+//  simple-income-tax-calculator
+//
+//  Created by Eric Popelka on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct simple_income_tax_calculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
